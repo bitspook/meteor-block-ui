d3a0f55bbcbf0c56c255936ae63411a2e7dc4f7f
Charanjit Singh <ckhabra007@gmail.com>
2014-06-26 22:43:40 +0530

    Restructured code better.

--- package.js
+++ package.js
@@ -6,9 +6,10 @@ Package.on_use(function (api, where) {
     api.use(['templating', 'ui','handlebars', 'deps', 'session'], 'client');
 
     api.add_files([
-        'lib/block_ui.css',
-        'lib/block_ui.html',
         'lib/block_ui.js',
+        'lib/block_ui.css',
+        'lib/templates.html',
+        'lib/templates.js',
     ], 'client');
 
     api.export && api.export(['BlockUI']);
